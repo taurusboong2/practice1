dfbff01677baa4507b7644ec13eac4245a6cee2e
taurusboong2 <taurusboong2@gmail.com>
2022-01-11 20:05:05 +0900

    delete fail

--- article1/article.js
+++ article1/article.js
@@ -1,6 +1,5 @@
-let url = 'http://localhost:1337/api/articles';
-
 const getData = async () => {
+    let url = 'http://localhost:1337/api/articles';
     try {
         const response = await fetch(url);
         if (response.ok) {
@@ -18,10 +17,10 @@ let myData = () => {
         let list = document.querySelector("#list");
         let data = res.data;
         let result = ``;
-        data.forEach(e => {
+        data.forEach((e, i) => {
             result += `
             <li>
-                <a href="article_detail.html">
+                <a href="article_detail.html?id=${i}">
                     <h2>${e.attributes.title}</h2>
                     <p>${e.attributes.createdAt}</p>
                 </a>
@@ -33,6 +32,8 @@ let myData = () => {
 }
 
 const putData = () => {
+    const url = new URL(window.location);
+    const id = url.searchParams.get("id");
     getData().then( res => {
         const data = res.data;
         const title = document.querySelector("#detail_title");
@@ -41,12 +42,45 @@ const putData = () => {
         const type = document.querySelector("#detail_type");
         const create = document.querySelector("#create_data");
         const update = document.querySelector("#update_data");
-
-        title.innerHTML = data[0].attributes.title;
-        type.innerHTML = data[0].attributes.type;
-        des.innerHTML = data[0].attributes.description;
-        author.innerHTML = data[0].attributes.author;
-        create.innerHTML = data[0].attributes.createdAt;
-        update.innerHTML = data[0].attributes.updatedAt;
+        data.forEach((e, i) => {
+            if(id == i) {
+                title.innerHTML = e.attributes.title;
+                type.innerHTML = e.attributes.type;
+                des.innerHTML = e.attributes.description;
+                author.innerHTML = e.attributes.author;
+                create.innerHTML = e.attributes.createdAt;
+                update.innerHTML = e.attributes.updatedAt;
+            }
+        })
     })
 }
+
+const deleteData1 = async () => {
+    try {
+        const response = await fetch('http://localhost:1337/api/articles', {
+            method:'DELETE',
+        })
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            console.log(response);
+            console.log(jsonResponse);
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const deleteData2 = () => {
+    getData().then(res => {
+        const data = res.data;
+        data.forEach(e => {
+            const id = e.id;
+            console.log('id는', id);
+        })
+    })
+}
+
+const deleteBtn = document.querySelector('#delete_btn');
+
+deleteData2();
+deleteData1();
